43f6fd0ade9f4abb054c60d134d1604072bd5e86
Amr20533 <amrelnemr638@gmail.com>
2025-07-24 17:20:14 +0300

    Initial commit

---
+++ questions/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+import uuid
+from django.conf import settings
+
+class IntroQuestion(models.Model):
+    SINGLE = 'single'
+    MULTIPLE = 'multiple'
+    QUESTION_TYPE_CHOICES = [
+        (SINGLE, 'Single Answer'),
+        (MULTIPLE, 'Multiple Answer'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question = models.CharField(max_length=500, default="", blank=True)
+    type = models.CharField(max_length=10, choices=QUESTION_TYPE_CHOICES, default=SINGLE)
+    description = models.TextField(blank=True, default="")
+
+    def __str__(self):
+        return self.question
+
+
+class IntroOption(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question = models.ForeignKey(IntroQuestion, related_name='options', on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    is_default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
+
+class IntroAnswer(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    question = models.ForeignKey(IntroQuestion, on_delete=models.CASCADE, related_name='answers')
+    # Single answer
+    text_answer = models.TextField(blank=True, null=True)
+    # For multiple-choice answers (or selected choice from single type)
+    selected_options = models.ManyToManyField(IntroOption, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'question')
+
+    def __str__(self):
+        return f"Answer by {self.user} to '{self.question}'"
